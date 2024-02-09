cc518e213df85b62617816d6b07f16d5e85f44a6
Guilherme O <guilhergoa@hotmail.com>
2024-02-08 23:03:37 -0300

    fix: fixing routers configs

--- api/routers/appRouter.py
+++ api/routers/appRouter.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from api.routers import test, analyzer
 
 app_routers = APIRouter()
-app_routers.include_router(test.router, prefix="/test")
-app_routers.include_router(analyzer.router, prefix="/analyzer")
+app_routers.include_router(test.router)
+app_routers.include_router(analyzer.router, prefix="/analyze")
