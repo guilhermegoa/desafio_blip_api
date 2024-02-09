9b0f98c66fd895749a7b811ac60cddde1a197888
Guilherme Oliveira <guilherme.antonio@take.net>
2024-02-09 09:00:16 -0300

    doc: adding document on model and service

--- api/models/analyzer.py
+++ api/models/analyzer.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel
 
 
 class AnalyzeSentiment(BaseModel):
+    """
+    Sentiment analysis result
+    """
     neg: float
     neu: float
     pos: float
--- api/services/analyzer.py
+++ api/services/analyzer.py
@@ -4,10 +4,24 @@ from api.models.analyzer import AnalyzeSentiment
 
 
 class Analyzer:
+    """
+    Classe responsável por realizar a análise de uma frase.
+    """
+
     def __init__(self):
         pass
 
     def analyzeSentiment(self, sentence: str) -> AnalyzeSentiment:
+        """
+        Analisa o sentimento da frase fornecida.
+
+        Args:
+            sentence (str): A frase para análise de sentimento.
+
+        Returns:
+            Dict[str, float]: Um dicionário contendo as pontuações de sentimento.
+                As chaves do dicionário são 'neg', 'neu', 'pos' e 'compound'.
+        """
         if not sentence or sentence == "" or type(sentence) is not str:
             raise Exception({"error": "No sentence provided"})
         
