76d44fab2cd54608f2eb2c1e52ba5c15005d1313
Guilherme O <guilhergoa@hotmail.com>
2024-02-08 23:03:47 -0300

    fix: add init on class

--- api/services/analyzer.py
+++ api/services/analyzer.py
@@ -2,15 +2,14 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 class Analyzer:
-    # def __init__(self, data):
-    #     self.data = data
+    def __init__(self):
+        pass
 
     def analyzeSentiment(self, sentence: str):
         if not sentence or sentence == "" or type(sentence) is not str:
             raise {"error": "No sentence provided"}
         
         sid = SentimentIntensityAnalyzer()
-        print(sentence)
         ss = sid.polarity_scores(sentence)
 
         return ss
